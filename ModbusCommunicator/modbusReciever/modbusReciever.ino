34cdc6a1f66cac5531666f5dd73f69f6a37e17d0
FreezinFire <grvt.shrm2003@gmail.com>
2024-10-15 10:55:06 +0530

    Read Serial input from sensor

--- ModbusCommunicator/modbusReciever/modbusReciever.ino
+++ ModbusCommunicator/modbusReciever/modbusReciever.ino
@@ -1,5 +1,6 @@
 #include <ModbusMaster.h>
 
+// DE and RE pin mapping
 #define MAX485_DE     23
 #define MAX485_RE_NEG 23
 
@@ -23,7 +24,7 @@ void setup(){
   digitalWrite(MAX485_DE, 0);
 
   Serial.begin(9600);
-  // Serial2.begin(9600);
+  // Set virtual node to device with ID 1
   node.begin(1, Serial);
 
   node.preTransmission(preTransmission);
@@ -32,18 +33,18 @@ void setup(){
 
 void loop(){
   uint8_t result;
-
+  //Request data from registers with temperature and himidity
   result = node.readInputRegisters(0x0001, 2);
 
   if(result == node.ku8MBSuccess){
+    // Display data with 1 decimal place accuracy
     Serial.print("Temperature: ");
     Serial.println(node.getResponseBuffer(0) / 10.0f);
     Serial.print("Humidity: ");
     Serial.println(node.getResponseBuffer(1) / 10.0f);
-    // Serial.println(result);
   } else {
+    // Print error code
     Serial.print("Modbus Error: ");
-    // Serial.println(node.getResponseBuffer(0));
     Serial.println(result);
   }
   delay(1000);
