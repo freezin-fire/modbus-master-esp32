2a127f96e1f06950b7377b915f5961ea3a03ccaa
FreezinFire <grvt.shrm2003@gmail.com>
2024-10-17 15:03:31 +0530

    Read data from WS200 sensor

--- ModbusCommunicator/modbusReciever/modbusReciever.ino
+++ ModbusCommunicator/modbusReciever/modbusReciever.ino
@@ -1,25 +1,25 @@
 #include <ModbusMaster.h>
 
 // DE and RE pin mapping
-#define MAX485_DE     23
+#define MAX485_DE 23
 #define MAX485_RE_NEG 23
 
 ModbusMaster node;
 
-void preTransmission(){
+void preTransmission() {
   digitalWrite(MAX485_RE_NEG, 1);
   digitalWrite(MAX485_DE, 1);
 }
 
-void postTransmission(){
+void postTransmission() {
   digitalWrite(MAX485_RE_NEG, 0);
   digitalWrite(MAX485_DE, 0);
 }
 
-void setup(){
+void setup() {
   pinMode(MAX485_RE_NEG, OUTPUT);
   pinMode(MAX485_DE, OUTPUT);
-  
+
   digitalWrite(MAX485_RE_NEG, 0);
   digitalWrite(MAX485_DE, 0);
 
@@ -31,12 +31,12 @@ void setup(){
   node.postTransmission(postTransmission);
 }
 
-void loop(){
+void loop() {
   uint8_t result;
   //Request data from registers with temperature and himidity
   result = node.readInputRegisters(0x0001, 2);
 
-  if(result == node.ku8MBSuccess){
+  if (result == node.ku8MBSuccess) {
     // Display data with 1 decimal place accuracy
     Serial.print("Temperature: ");
     Serial.println(node.getResponseBuffer(0) / 10.0f);
---
+++ ModbusCommunicator/weatherStation/weatherStation.ino
@@ -0,0 +1,73 @@
+#include <ModbusMaster.h>
+
+// DE and RE pin mapping
+#define MAX485_DE 23
+#define MAX485_RE_NEG 23
+
+ModbusMaster node;
+
+void preTransmission() {
+  digitalWrite(MAX485_RE_NEG, 1);
+  digitalWrite(MAX485_DE, 1);
+}
+
+void postTransmission() {
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_DE, 0);
+}
+
+void setup() {
+  pinMode(MAX485_RE_NEG, OUTPUT);
+  pinMode(MAX485_DE, OUTPUT);
+
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_DE, 0);
+
+  Serial.begin(9600);
+  // Set virtual node to device with ID 1
+  node.begin(1, Serial);
+
+  node.preTransmission(preTransmission);
+  node.postTransmission(postTransmission);
+}
+
+void loop() {
+  uint8_t result;
+  
+  // Reading the 8 Holding registers according to the sensor specifications
+  result = node.readHoldingRegisters(0x0000, 8);
+
+  if (result == node.ku8MBSuccess) {
+
+    Serial.println("-------------------------------------");
+    Serial.print("Wind direction: ");
+    Serial.print(node.getResponseBuffer(0));
+    Serial.println(" Deg");
+    Serial.print("Wind Speed: ");
+    Serial.print(node.getResponseBuffer(1));
+    Serial.println(" kmph");
+    Serial.print("Amb Temp: ");
+    Serial.print(node.getResponseBuffer(2) / 10.0f);
+    Serial.println(" Deg C");
+    Serial.print("Humidity: ");
+    Serial.print(node.getResponseBuffer(3) / 10.0f);
+    Serial.println(" Per.");
+    Serial.print("Pyranometer: ");
+    Serial.print(node.getResponseBuffer(4));
+    Serial.println(" w/mm2");
+    Serial.print("Rain: ");
+    Serial.print(node.getResponseBuffer(5));
+    Serial.println(" mm");
+    Serial.print("Mod Temperature: ");
+    Serial.print(node.getResponseBuffer(6) / 10.0f);
+    Serial.println(" Deg C");
+    Serial.print("Barometer Pressure: ");
+    Serial.print(node.getResponseBuffer(7) / 10.0f);
+    Serial.println(" hpa");
+  } else {
+    // Print error code
+    Serial.print("Modbus Error: ");
+    Serial.println(result);
+  }
+  delay(1000);
+}
