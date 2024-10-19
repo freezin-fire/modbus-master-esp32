680b0bf347ffac2a4e41a389d03f102d2e232efb
FreezinFire <grvt.shrm2003@gmail.com>
2024-10-19 16:20:56 +0530

    Web Server API implementation

--- ModbusCommunicator/weatherStationWeb/weatherStationWeb.ino
+++ ModbusCommunicator/weatherStationWeb/weatherStationWeb.ino
@@ -1,18 +1,17 @@
 #include <WiFi.h>
-#include <WiFiClientSecure.h>
 #include <HTTPClient.h>
 #include <ModbusMaster.h>
 
 // Replace with your Wi-Fi SSID and password
-const char* ssid = "REPLACE_WITH_YOUR_SSID";
-const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+const char* ssid = "Airtel_kris_9998";
+const char* password = "air23985";
 
 // REPLACE with your Domain name and URL path or IP address with path
-const char* serverName = "http://example.com/esp-post-data.php";
+const char* serverName = "http://weatherdata.free.nf/esp-post-data.php";
 
 // Keep this API Key value to be compatible with the PHP code.
 // If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key
-String apiKeyValue = "tPmAT5Ab3j7F9";
+String apiKeyValue = "tPmAT5bgfe40536l";
 
 String sensor_status = "Offline";
 
@@ -60,12 +59,11 @@ void setup() {
 
 void loop() {
   if (WiFi.status() == WL_CONNECTED) {
-    WiFiClientSecure* client = new WiFiClientSecure;
-    client->setInsecure();  //don't use SSL certificate
+    WiFiClient client;
     HTTPClient http;
 
     // Your Domain name with URL path or IP address with path
-    http.begin(*client, serverName);
+    http.begin(client, serverName);
 
     // Specify content-type header
     http.addHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -81,12 +79,12 @@ void loop() {
       httpRequestData = "api_key=" + apiKeyValue + "&sensor_status=" + sensor_status
                         + "&wind_dir=" + String(node.getResponseBuffer(0))
                         + "&wind_speed=" + String(node.getResponseBuffer(1))
-                        + "&ambnt_temp=" + String(node.getResponseBuffer(2))
-                        + "&humidity=" + String(node.getResponseBuffer(3))
+                        + "&ambnt_temp=" + String(node.getResponseBuffer(2) / 10.0f)
+                        + "&humidity=" + String(node.getResponseBuffer(3) / 10.0f)
                         + "&pyranometer=" + String(node.getResponseBuffer(4))
                         + "&rain=" + String(node.getResponseBuffer(5))
-                        + "&mod_temp=" + String(node.getResponseBuffer(6))
-                        + "&barometer=" + String(node.getResponseBuffer(7))
+                        + "&mod_temp=" + String(node.getResponseBuffer(6) / 10.0f)
+                        + "&barometer=" + String(node.getResponseBuffer(7) / 10.0f)
                         + "";
 
       Serial.print("Wind direction: ");
@@ -134,6 +132,8 @@ void loop() {
     Serial.print("httpRequestData: ");
     Serial.println(httpRequestData);
 
+    int httpResponseCode = http.POST(httpRequestData);
+
     if (httpResponseCode > 0) {
       Serial.print("HTTP Response code: ");
       Serial.println(httpResponseCode);
@@ -147,5 +147,5 @@ void loop() {
     Serial.println("WiFi Disconnected");
   }
 
-  delay(30000);
+  delay(15000);
 }
